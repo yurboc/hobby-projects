c71028f00d2e07b6d860e00cf9b1c2565d1b0990
Yuriy Botsev <yurboc@yandex.ru>
2017-12-18 05:08:58 +0300

    First release of SimpleTimerLcd

--- arduino/SimpleTimerLcd/SimpleTimerLcd.ino
+++ arduino/SimpleTimerLcd/SimpleTimerLcd.ino
@@ -9,14 +9,28 @@
 #include <RtcDS3231.h>
 #include "custom_symbols.h"
 
+//
+// Types
+//
 enum ModeGlobal {
-  ModeTesting,
-  ModeClock,
-  ModeTimer,
-  ModeSetup
+  ModeTesting = 0x0000,
+  ModeClock   = 0x0080,
+  ModeTimer   = 0x8000,
+  ModeSetCurrentDay     = 0x0001,
+  ModeSetCurrentMonth   = 0x0002,
+  ModeSetCurrentYear    = 0x0004,
+  ModeSetCurrentHours   = 0x0008,
+  ModeSetCurrentMinutes = 0x0010,
+  ModeSetCurrentSeconds = 0x0020,
+  ModeSetTimerHours     = 0x0100,
+  ModeSetTimerMinutes   = 0x0200,
+  ModeSetTimerSeconds   = 0x0400,
+  ModeTimerRun          = 0x0800,
 };
 
+//
 // Hardware definitions
+//
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
 RtcDS3231<TwoWire> rtc(Wire);
 const int encA   = 2;  // Encoder out A
@@ -27,15 +41,64 @@ const int ledRed = 11;     // Red LED
 const int ledYellow = 10;  // Yellow LED
 const int ledGreen = 9;    // Green LED
 
+//
 // Global state
+//
 ModeGlobal currentMode = ModeTesting;
-unsigned long lastUpdated = 0;
+
+//
+// Button state (with debounce)
+//
+int buttonState = HIGH;
+int lastButtonState = HIGH;
+unsigned long lastDebounceTime = 0;
+boolean enterSettings = false;
+
+//
+// Encoder states
+//
+volatile int encoder0Pos = 0;
+volatile boolean PastA = 0;
+volatile boolean PastB = 0;
+
+//
+// Current time (updates from RTC every 1 second)
+//
+int seconds = 0;
+int minutes = 0;
+int hours   = 0;
+int day     = 0;
+int month   = 0;
+int year    = 0;
+int temp    = 0;
+unsigned long lastUpdatedRtc = 0;
+unsigned long lastUpdatedTemplate = 0;
+
+//
+// Current timer
+//
+int init_timer_seconds = 0;
+int init_timer_minutes = 0;
+int init_timer_hours   = 0;
+int timer_seconds = 0;
+int timer_minutes = 0;
+int timer_hours   = 0;
+int lastRegisteredSecond = 0;
+int beepTime = 0;
 
 void setup()
 {
   // LCD setup
   lcd.begin();
   lcd.noBacklight();
+  lcd.createChar(0, degree);
+  lcd.createChar(1, charge_damage);
+  lcd.createChar(2, charge_empty);
+  lcd.createChar(3, charge_half);
+  lcd.createChar(4, charge_full);
+  lcd.createChar(5, charge_usb);
+  lcd.createChar(6, black_square);
+  
   
   // RTC setup
   rtc.Begin();
@@ -57,6 +120,15 @@ void setup()
   pinMode(ledYellow, INPUT);
   pinMode(ledGreen,  INPUT);
 
+  // Setup Timer to read encoder
+  cli();
+  TCCR1A = 0;
+  TCCR1B = (1 << WGM12);
+  TCCR1B |= (1 << CS10);
+  TIMSK |= (1 << OCIE1A);
+  OCR1A = 8191;
+  sei();
+
   // System check
   systemCheck();
 
@@ -66,144 +138,245 @@ void setup()
 
 void loop()
 {
-  // Show clock
-  if (currentMode == ModeClock) {
-    if (millis() - lastUpdated < 500)
-      return;
+  // Read encoder value
+  readEncoder();
+  
+  // Show template
+  showTemplate();
+
+  // Update current time every 0.5 second
+  if ((currentMode == ModeClock || currentMode == ModeTimer || currentMode == ModeTimerRun) && millis() - lastUpdatedRtc > 500) {
+    lastUpdatedRtc = millis();
     
-    templateShowClock();
-    lastUpdated = millis();
+    RtcDateTime now = rtc.GetDateTime();
+    temp    = rtc.GetTemperature().AsWholeDegrees();
+    day     = now.Day();
+    month   = now.Month();
+    year    = now.Year();
+    hours   = now.Hour();
+    minutes = now.Minute();
+    seconds = now.Second();
+
+    // Update timer
+    if (lastRegisteredSecond != seconds && currentMode == ModeTimerRun) {
+      updateTimerTimeout();
+
+      // Timer pre-alert
+      if (timer_hours == 0 && timer_minutes == 0 && (timer_seconds <= 10 && timer_seconds > 0)) {
+        greenOn(); delay(100); greenOff(); delay(50); greenOn(); delay(100); greenOff();
+      }
+      else if (timer_hours == 0 && timer_minutes == 0 && (timer_seconds <= 30 && timer_seconds > 10)) {
+        greenOn(); delay(50); greenOff();
+      }
+      
+      lastRegisteredSecond = seconds;
+    }
+
+    // Timer beep
+    if (beepTime > 0 && seconds % 2 == 0) {
+      yellowOn();
+      beepOn(); delay(100); beepOff(); delay(50); beepOn(); delay(100); beepOff();
+      yellowOff();
+
+      if (--beepTime == 0) {
+        currentMode = ModeTimer;
+      }
+    }
+
+    // Check health state
+    if (lcd.getBacklight() && !hasUsb()) {
+        lcd.noBacklight();
+    }
+    if (battState() == 1) {
+      redOn(); delay(50); redOff(); delay(10); redOn(); delay(50); redOff();
+    }
+
+    // Change mode
+    if (encoder0Pos > 3 && currentMode == ModeClock)
+      currentMode = ModeTimer;
+    else if (encoder0Pos < -3 && currentMode == ModeTimer)
+      currentMode = ModeClock;
+    encoder0Pos = 0;
+  }
+  
+  // Detect button click
+  int currentButtonState = digitalRead(encKey);
+  if (currentButtonState != lastButtonState) {
+    lastDebounceTime = millis();
+    lastButtonState = currentButtonState;
+  }
+  if ((millis() - lastDebounceTime) > 50 && (currentButtonState != buttonState)) {
+    if (buttonState == LOW && currentButtonState == HIGH) {
+      beepOn();
+      delay(10);
+      beepOff();
+      currentMode = nextMode();
+    }
+    buttonState = currentButtonState;
   }
 
-  // Show timer
-  if (currentMode == ModeTimer) {
-    if (millis() - lastUpdated < 500)
-      return;
-    
-    templateShowTimer();
-    lastUpdated = millis();
+  // Detect button hold
+  if ((millis() - lastDebounceTime) > 1500 && (currentButtonState == LOW)) {
+    if (!enterSettings) {
+      beepOn();
+      delay(50);
+      beepOff();
+    }
+    enterSettings = true;
   }
-
-  // Read encoder
 }
 
-void systemCheck()
+
+
+void readEncoder()
 {
-  // Load symbols
-  lcd.createChar(0, bukva_P);
-  lcd.createChar(1, bukva_I);
-  lcd.createChar(2, bukva_IYI);
-  lcd.createChar(3, bukva_Z);
-  lcd.createChar(4, bukva_U);
-  lcd.createChar(5, check);
-  lcd.createChar(6, check);
-  lcd.createChar(7, check);
+  if (encoder0Pos == 0) return;
 
-  // Print a message "SYSTEM CHECK"
-  lcd.home();
-  lcd.write(0); // bukva_P
-  lcd.print("POBEPKA C");
-  lcd.write(1); // bukva_I
-  lcd.print("CTEM");
-  lcd.write(2); // bukva_IYI
-  delay(100);
+  // Setup current time
+  if (currentMode == ModeSetCurrentDay)          updateEncoderValueWithLimits(day, 1, 31);
+  else if (currentMode == ModeSetCurrentMonth)   updateEncoderValueWithLimits(month, 1, 12);
+  else if (currentMode == ModeSetCurrentYear)    updateEncoderValueWithLimits(year, 2000, 2100);
+  else if (currentMode == ModeSetCurrentHours)   updateEncoderValueWithLimits(hours, 0, 24);
+  else if (currentMode == ModeSetCurrentMinutes) updateEncoderValueWithLimits(minutes, 0, 59);
+  else if (currentMode == ModeSetCurrentSeconds) updateEncoderValueWithLimits(seconds, 0, 59);
+
+  // Setup timer
+  else if (currentMode == ModeSetTimerHours)   updateEncoderValueWithLimits(timer_hours, 0, 99);
+  else if (currentMode == ModeSetTimerMinutes) updateEncoderValueWithLimits(timer_minutes, 0, 59);
+  else if (currentMode == ModeSetTimerSeconds) updateEncoderValueWithLimits(timer_seconds, 0, 59);
+}
+
+void updateEncoderValueWithLimits(int &value, int lower, int upper)
+{
+  value += encoder0Pos;
   
-  // Check buzzer
-  lcd.setCursor(0, 1);
-  lcd.print("   ");
-  lcd.write(3); // bukva_Z
-  lcd.print("B");
-  lcd.write(4); // bukva_U
-  lcd.print("K ");
-  delay(100);
-  beepOn();
-  lcd.print("....... ");
-  delay(100);
-  beepOff();
+  if (value > upper) value = upper;
+  else if (value < lower) value = lower;
 
-  // Check LEDs
-  lcd.setCursor(0, 1);
-  lcd.print("                ");
-  delay(200);
+  encoder0Pos = 0;
+}
+
+void updateTimerTimeout()
+{
+  // Timer timeout
+  if (!timer_hours && !timer_minutes && !timer_seconds && currentMode == ModeTimerRun && beepTime == 0) {
+    beepTime = 10;
+    return;
+  }
+
+  // Decrease timer counter
+  if (lastRegisteredSecond != seconds) {
+    if (timer_seconds > 0) {
+      --timer_seconds;
+      return;
+    } else if (timer_minutes > 0) {
+      --timer_minutes;
+      timer_seconds = 59;
+      return;
+    } else if (timer_hours > 0) {
+      --timer_hours;
+      timer_minutes = 59;
+      timer_seconds = 59;
+      return;
+    }
+  }
+}
+
+void showTemplate()
+{
+  if (enterSettings && millis() - lastUpdatedTemplate < 300)
+    return;
+
+  if (!enterSettings && millis() - lastUpdatedTemplate < 1000)
+    return;
   
-  // Red
-  lcd.setCursor(0, 1);
-  lcd.print(" CBET ...");
-  redOn();
-  delay(350);
-  redOff();
+  if (currentMode & 0x00FF) templateShowClock();
+  if (currentMode & 0xFF00) templateShowTimer();
 
-  // Yellow
-  lcd.print("...");
-  yellowOn();
-  delay(350);
-  yellowOff();
-
-  // Green
-  lcd.print("...");
-  greenOn();
-  delay(350);
-  greenOff();
-
-  // Done
-  lcd.setCursor(0, 1);
-  lcd.print(" ===== OK ===== ");
-  delay(500);
-  lcd.clear();
+  lastUpdatedTemplate = millis();
 }
 
 void templateShowClock()
 {
-  // Load symbols
-  lcd.createChar(0, degree);
-  lcd.createChar(1, charge_damage);
-  lcd.createChar(2, charge_empty);
-  lcd.createChar(3, charge_half);
-  lcd.createChar(4, charge_full);
-  lcd.createChar(5, charge_usb);
-  lcd.createChar(6, check);
-  lcd.createChar(7, check);
-
-  // Read values
-  RtcDateTime now = rtc.GetDateTime();
-  int temp    = rtc.GetTemperature().AsWholeDegrees();
-  int day     = now.Day();
-  int month   = now.Month();
-  int year    = now.Year();
-  int hours   = now.Hour();
-  int minutes = now.Minute();
-  int seconds = now.Second();
-  
   // Line 1: [DD.MM.YYYY +##*C]
   lcd.home();
-  lcd.write('0' + day/10);
-  lcd.write('0' + day%10);
+  
+  if (enterSettings && currentMode != ModeSetCurrentDay) {
+    lcd.write('_');
+    lcd.write('_');
+  }
+  else {
+    lcd.write('0' + day/10);
+    lcd.write('0' + day%10);
+  }
   lcd.write('.');
-  lcd.write('0' + month/10);
-  lcd.write('0' + month%10);
+  if (enterSettings && currentMode != ModeSetCurrentMonth) {
+    lcd.write('_');
+    lcd.write('_');
+  }
+  else {
+    lcd.write('0' + month/10);
+    lcd.write('0' + month%10);
+  }
   lcd.write('.');
-  lcd.write('0' + year/1000%10);
-  lcd.write('0' + year/100%10);
-  lcd.write('0' + year/10%10);
-  lcd.write('0' + year%10);
-  lcd.write(' ');
-  lcd.write(temp > 0 ? '+' : '-');
-  lcd.write('0' + temp/10);
-  lcd.write('0' + temp%10);
-  lcd.write(0); // degree
-  lcd.write('C');
+  if (enterSettings && currentMode != ModeSetCurrentYear) {
+    lcd.write('_');
+    lcd.write('_');
+    lcd.write('_');
+    lcd.write('_'); }
+  else {
+    lcd.write('0' + year/1000%10);
+    lcd.write('0' + year/100%10);
+    lcd.write('0' + year/10%10);
+    lcd.write('0' + year%10);
+  }
+  
+  if (!enterSettings) {
+    lcd.write(' ');
+    lcd.write(temp > 0 ? '+' : '-');
+    lcd.write('0' + temp/10);
+    lcd.write('0' + temp%10);
+    lcd.write(0); // degree
+    lcd.write('C');
+  }
+  else {
+    lcd.write(' ');
+    lcd.write(' ');
+    lcd.write(' ');
+    lcd.write(' ');
+    lcd.write(' ');
+    lcd.write(' ');
+  }
 
   // Line 2: [    HH:MM:SS  uc]
   lcd.setCursor(0, 1);
   lcd.print("    ");
-  lcd.write('0' + hours/10);
-  lcd.write('0' + hours%10);
+  if (enterSettings && currentMode != ModeSetCurrentHours) {
+    lcd.write('_');
+    lcd.write('_');
+  }
+  else {
+    lcd.write('0' + hours/10);
+    lcd.write('0' + hours%10);
+  }
   lcd.write(':');
-  lcd.write('0' + minutes/10);
-  lcd.write('0' + minutes%10);
+  if (enterSettings && currentMode != ModeSetCurrentMinutes) {
+    lcd.write('_');
+    lcd.write('_');
+  }
+  else {
+    lcd.write('0' + minutes/10);
+    lcd.write('0' + minutes%10);
+  }
   lcd.write(':');
-  lcd.write('0' + seconds/10);
-  lcd.write('0' + seconds%10);
+  if (enterSettings && currentMode != ModeSetCurrentSeconds) {
+    lcd.write('_');
+    lcd.write('_');
+  }
+  else {
+    lcd.write('0' + seconds/10);
+    lcd.write('0' + seconds%10);
+  }
   lcd.print("  ");
 
   // Print battery status
@@ -215,36 +388,41 @@ void templateShowClock()
 
 void templateShowTimer()
 {
-  // Load symbols
-  lcd.createChar(0, degree);
-  lcd.createChar(1, charge_damage);
-  lcd.createChar(2, charge_empty);
-  lcd.createChar(3, charge_half);
-  lcd.createChar(4, charge_full);
-  lcd.createChar(5, charge_usb);
-  lcd.createChar(6, check);
-  lcd.createChar(7, check);
-
-  // Read values
-  int hours   = 0;
-  int minutes = 0;
-  int seconds = 0;
-
   // Line 1: [################]
   lcd.home();
-  lcd.print("################");
+  for (int i = 0; i < 16; ++i) {
+    if (currentMode == ModeTimerRun) lcd.write('>');
+    else lcd.write(6); // black_square
+  }
 
-  // Line 2: [    HH:MM:SS  uc]
-  lcd.setCursor(0, 1);
-  lcd.print("    ");
-  lcd.write('0' + hours/10);
-  lcd.write('0' + hours%10);
+  // Line 2: [    HH:MM:SS    ]
+  lcd.setCursor(4, 1);
+  if (enterSettings && currentMode != ModeSetTimerHours) {
+    lcd.write('_');
+    lcd.write('_');
+  }
+  else {
+    lcd.write('0' + timer_hours/10);
+    lcd.write('0' + timer_hours%10);
+  }
   lcd.write(':');
-  lcd.write('0' + minutes/10);
-  lcd.write('0' + minutes%10);
+  if (enterSettings && currentMode != ModeSetTimerMinutes) {
+    lcd.write('_');
+    lcd.write('_');
+  }
+  else {
+    lcd.write('0' + timer_minutes/10);
+    lcd.write('0' + timer_minutes%10);
+  }
   lcd.write(':');
-  lcd.write('0' + seconds/10);
-  lcd.write('0' + seconds%10);
+  if (enterSettings && currentMode != ModeSetTimerSeconds) {
+    lcd.write('_');
+    lcd.write('_');
+  }
+  else {
+    lcd.write('0' + timer_seconds/10);
+    lcd.write('0' + timer_seconds%10);
+  }
   lcd.print("  ");
 
   // Print battery status
@@ -252,6 +430,65 @@ void templateShowTimer()
   int batt = battState(); // 0 or 1..4
   if (batt == 0) lcd.write(' '); // none
   else lcd.write(batt); // damage/empty/half/full
+}
+
+int nextMode()
+{
+  switch (currentMode) {
+    case ModeClock: {
+      if (enterSettings) return ModeSetCurrentDay;
+      if (lcd.getBacklight()) {
+        lcd.noBacklight();
+      }
+      else if (hasUsb()) {
+        lcd.backlight();
+      }
+      return ModeClock;
+    }
+    case ModeTimer: {
+      if (enterSettings) return ModeSetTimerHours;
+      if (!timer_hours && !timer_minutes && !timer_seconds)
+        return ModeTimer;
+      else
+        return ModeTimerRun;
+    }
+    case ModeTimerRun: {
+      if (enterSettings) {
+        timer_seconds = init_timer_seconds;
+        timer_minutes = init_timer_minutes;
+        timer_hours   = init_timer_hours;
+        enterSettings = false;
+      }
+      beepTime = 0;
+      return ModeTimer;
+    }
+    
+    case ModeSetCurrentDay: return ModeSetCurrentMonth;
+    case ModeSetCurrentMonth: return ModeSetCurrentYear;
+    case ModeSetCurrentYear: return ModeSetCurrentHours;
+    case ModeSetCurrentHours: return ModeSetCurrentMinutes;
+    case ModeSetCurrentMinutes: return ModeSetCurrentSeconds;
+    case ModeSetCurrentSeconds: {
+      // Save RTC
+      RtcDateTime newDateTime = RtcDateTime(year, month, day, hours, minutes, seconds);
+      rtc.SetDateTime(newDateTime);
+      enterSettings = false;
+      return ModeClock;
+    }
+
+    case ModeSetTimerHours: return ModeSetTimerMinutes;
+    case ModeSetTimerMinutes: return ModeSetTimerSeconds;
+    case ModeSetTimerSeconds: {
+      // Save Timer settings
+      init_timer_seconds = timer_seconds;
+      init_timer_minutes = timer_minutes;
+      init_timer_hours   = timer_hours;
+      enterSettings = false;
+      return ModeTimer;
+    }
+    
+    default: return ModeClock;
+  }
 }
 
 
@@ -326,3 +563,86 @@ int battState()
     return 0;
 }
 
+
+
+void systemCheck()
+{
+  /*
+  // Load symbols
+  lcd.createChar(0, bukva_P);
+  lcd.createChar(1, bukva_I);
+  lcd.createChar(2, bukva_IYI);
+  lcd.createChar(3, bukva_Z);
+  lcd.createChar(4, bukva_U);
+
+  // Print a message "SYSTEM CHECK"
+  lcd.home();
+  lcd.write(0); // bukva_P
+  lcd.print("POBEPKA C");
+  lcd.write(1); // bukva_I
+  lcd.print("CTEM");
+  lcd.write(2); // bukva_IYI
+  delay(100);
+  
+  // Check buzzer
+  lcd.setCursor(0, 1);
+  lcd.print("   ");
+  lcd.write(3); // bukva_Z
+  lcd.print("B");
+  lcd.write(4); // bukva_U
+  lcd.print("K ");
+  delay(100);
+  beepOn();
+  delay(100);
+  beepOff();
+
+  // Check LEDs
+  lcd.setCursor(0, 1);
+  delay(200);
+  
+  // Red
+  lcd.setCursor(0, 1);
+  lcd.print(" CBET ...");
+  redOn();
+  delay(350);
+  redOff();
+
+  // Yellow
+  lcd.print("...");
+  yellowOn();
+  delay(350);
+  yellowOff();
+
+  // Green
+  lcd.print("...");
+  greenOn();
+  delay(350);
+  greenOff();
+
+  // Done
+  lcd.setCursor(0, 1);
+  lcd.print(" ===== OK ===== ");
+  delay(500);
+  lcd.clear();
+  */
+}
+
+
+
+//
+// ===== ISR =====
+//
+ISR(TIMER1_COMPA_vect)
+{
+  /* Read encoder */
+  boolean nowA = (boolean)digitalRead(encA);
+  boolean nowB = (boolean)digitalRead(encB);
+  if (!PastA && nowA) {
+    if (!nowB)
+      encoder0Pos--;
+    else
+      encoder0Pos++;
+  }
+  PastA = nowA;
+  PastB = nowB;
+}
