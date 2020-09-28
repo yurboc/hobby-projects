c62c5e1f24e4e07d30060cf28d6c1c059c271822
Yuriy Botsev <u.botsev@atol.ru>
2020-09-28 13:31:45 +0300

    FST-OTM: add telegram_api.php

---
+++ fst-otm/telegram_api.php
@@ -0,0 +1,140 @@
+<?php
+
+require __DIR__ . '/credentials.php';
+
+function exec_curl_request($handle) {
+  $response = curl_exec($handle);
+
+  if ($response === false) {
+    $errno = curl_errno($handle);
+    $error = curl_error($handle);
+    error_log("Curl returned error $errno: $error\n");
+    curl_close($handle);
+    return false;
+  }
+
+  $http_code = intval(curl_getinfo($handle, CURLINFO_HTTP_CODE));
+  curl_close($handle);
+
+  if ($http_code >= 500) {
+    // do not wat to DDOS server if something goes wrong
+    sleep(10);
+    return false;
+  } else if ($http_code != 200) {
+    $response = json_decode($response, true);
+    error_log("Request has failed with error {$response['error_code']}: {$response['description']}\n");
+    if ($http_code == 401) {
+      throw new Exception('Invalid access token provided');
+    }
+    return false;
+  } else {
+    $response = json_decode($response, true);
+    if (isset($response['description'])) {
+      error_log("Request was successful: {$response['description']}\n");
+    }
+    $response = $response['result'];
+  }
+
+  return $response;
+}
+
+function apiRequestWebhook($method, $parameters) {
+  if (!is_string($method)) {
+    error_log("Method name must be a string\n");
+    return false;
+  }
+
+  if (!$parameters) {
+    $parameters = array();
+  } else if (!is_array($parameters)) {
+    error_log("Parameters must be an array\n");
+    return false;
+  }
+
+  $parameters["method"] = $method;
+
+  header("Content-Type: application/json");
+  echo json_encode($parameters);
+  return true;
+}
+
+function apiRequest($method, $parameters) {
+  if (!is_string($method)) {
+    error_log("Method name must be a string\n");
+    return false;
+  }
+
+  if (!$parameters) {
+    $parameters = array();
+  } else if (!is_array($parameters)) {
+    error_log("Parameters must be an array\n");
+    return false;
+  }
+
+  foreach ($parameters as $key => &$val) {
+    // encoding to JSON array parameters, for example reply_markup
+    if (!is_numeric($val) && !is_string($val)) {
+      $val = json_encode($val);
+    }
+  }
+
+  global $telegramApiUrl;
+  $url = $telegramApiUrl.$method.'?'.http_build_query($parameters);
+
+  $handle = curl_init($url);
+  curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+  curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+
+  return exec_curl_request($handle);
+}
+
+function apiRequestJson($method, $parameters) {
+  if (!is_string($method)) {
+    error_log("Method name must be a string\n");
+    return false;
+  }
+
+  if (!$parameters) {
+    $parameters = array();
+  } else if (!is_array($parameters)) {
+    error_log("Parameters must be an array\n");
+    return false;
+  }
+
+  $parameters["method"] = $method;
+
+  $handle = curl_init($telegramApiUrl);
+  curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+  curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+  curl_setopt($handle, CURLOPT_POST, true);
+  curl_setopt($handle, CURLOPT_POSTFIELDS, json_encode($parameters));
+  curl_setopt($handle, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+
+  return exec_curl_request($handle);
+}
+
+function notifySendDone($name, $time) {
+  global $telegramChatId;
+  apiRequest("sendMessage", array(
+    'chat_id' => $telegramChatId,
+    'text' => "\xE2\x9C\x94 *".$name."*: выгружены за *".number_format($time, 2, ',', '')."* мс."
+      ."\nclient ip: ".$_SERVER['REMOTE_ADDR']
+      ."\nuser agent: ".$_SERVER['HTTP_USER_AGENT'],
+    'parse_mode' => 'markdown'
+  ));
+}
+
+function notifySendFail($name, $text) {
+  global $telegramChatId;
+  apiRequest("sendMessage", array(
+    'chat_id' => $telegramChatId,
+    'text' => "\xE2\x9D\x8C *".$name."*: ошибка: *".$text."*"
+      ."\nclient ip: ".$_SERVER['REMOTE_ADDR']
+      ."\nuser agent: ".$_SERVER['HTTP_USER_AGENT'],
+    'parse_mode' => 'markdown'
+  ));
+}
+
+?>
